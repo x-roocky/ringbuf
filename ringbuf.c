4e57dbd463090dbe69183fd43defc64c4569594d
王相宇 <wangxy@roocky.com>
2020-06-29 15:29:53 +0800

    refactor: 整理内部代码

--- ringbuf.c
+++ ringbuf.c
@@ -1,12 +1,13 @@
 #include "ringbuf.h"
 #include <memory.h>
 
-static inline size_t unused(const struct ringbuf *p) {
-    return p->mask - ((p->tail - p->head) & p->mask);
-}
-
 static inline size_t min(size_t a, size_t b) {
     return a > b ? b : a; 
+}
+
+static inline size_t skip(struct ringbuf *p, size_t length) {
+    p->head = (p->head + length) & p->mask;
+	return length;
 }
 
 void ringbuf_init(struct ringbuf *p, void *data, size_t size) {
@@ -17,31 +18,28 @@ void ringbuf_init(struct ringbuf *p, void *data, size_t size) {
 }
 
 size_t ringbuf_put(struct ringbuf *p, const void *data, size_t length) {
-    size_t tmp = unused(p);
-	if (length > tmp)
-		length = tmp;
+    size_t first;
 
-	tmp = min(length, p->mask - p->tail + 1);
-	memcpy(&p->data[p->tail], data, tmp);
-	memcpy(p->data, data + tmp, length - tmp);
+	length = min(length, ringbuf_size(p) - ringbuf_length(p));
+	first = min(length, p->mask - p->tail + 1);
+	memcpy(p->data +p->tail, data, first);
+	memcpy(p->data, data + first, length - first);
 
 	p->tail = (p->tail + length) & p->mask;
 	return length;
 }
 
 size_t ringbuf_peek(const struct ringbuf *p, void *buffer, size_t size) {
-    size_t tmp = ringbuf_length(p);
-	if (size > tmp)
-		size = tmp;
+    size_t first;
 
-	tmp = min(size, p->mask - p->head + 1);
-	memcpy(buffer, p->data + p->head, tmp);
-	memcpy(buffer + tmp, p->data, size - tmp);
+	size = min(size, ringbuf_length(p));
+	first = min(size, p->mask - p->head + 1);
+	memcpy(buffer, p->data + p->head, first);
+	memcpy(buffer + first, p->data, size - first);
+
     return size;
 }
 
 size_t ringbuf_get(struct ringbuf *p, void *buffer, size_t size) {
-    size = ringbuf_peek(p, buffer, size);
-    p->head = (p->head + size) & p->mask;
-	return size;
+	return skip(p, ringbuf_peek(p, buffer, size));
 }
